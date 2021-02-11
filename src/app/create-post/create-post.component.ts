df7d31fadd3f2ccf7348ef845a9f841ceab12ef4
Dmytro Sliusarenko <ds.sliusarenko@gmail.com>
2021-02-11 11:44:52 +0200

    temp commit

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-create-post',
-  templateUrl: 'create-post.component.html'
+  templateUrl: 'create-post.component.html',
+  styleUrls: ['create-post.component.css']
 })
 export class CreatePostComponent {
   constructor() {
